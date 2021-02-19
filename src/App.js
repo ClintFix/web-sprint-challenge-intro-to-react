ee6b7af0bfa3b45ae1b40caa21e98a66da3f9f5a
ClintFix <clint.fix@gmail.com>
2021-02-19 09:50:23 -0700

    got axios api call working. Returns array of pages of characters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Axios from 'axios'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [ characters, setCharacters ] = useState([]);
+  const [ page, setPage ] = useState(1)
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    Axios.get(`http://swapi.dev/api/people/?page=${page}`)
+      .then(res => {
+        let charsArray = characters;
+        charsArray.push(res.data.results)
+        setCharacters(charsArray)
+         if (res.data.next !== null) {
+           setPage(page + 1)
+         }
+      })
+      .catch(err => {
+        alert(`Axios error - line 15`)
+      })
+  },[page])
 
   return (
     <div className="App">
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,3 +1,7 @@
 // Write your Character component here
 import React, { useSTate, useEffect } from 'react'
 import styled from 'styled-components'
+
+export default function Characters(props) {
+    
+}
