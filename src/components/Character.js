b8a7b2aa17821f773d9a5fbf452220cf305b78a4
ClintFix <clint.fix@gmail.com>
2021-02-19 13:38:40 -0700

    basic styling done

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,45 +1,64 @@
 // Write your Character component here
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
+
+const CharactersList = styled.div`
+    width: 70%;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: column wrap;
+    align-items: center;
+`
+
+const CharacterCard = styled.div `
+    width: 400px;
+    box-shadow: 2px 2px 2px gray;
+    background-color: rgb(169,169,169, .8);
+    margin-bottom: 10px;
+`
+
+const Details = styled.div `
+    font-size: 14px;
+`
+
+const Buttons = styled.button `
+    width: 100px;
+    margin: 20px 20px 100px;
+`
 
 export default function Characters(props) {
     const { page, setPage, characters } = props;
-
     
     return (
-        <div>
-            <div>
+        <CharactersList>
                 {
                     characters.map(char => {
                         return (
-                            <div>
+                            <CharacterCard>
                                 <h3>{char.name}</h3>
-                                <div>
-                                    <ul>
-                                        <li>
-                                            {char.birth_year}
-                                        </li>
-                                        <li>
-                                            {char.gender}
-                                        </li>
-                                        <li>
-                                            {char.height}
-                                        </li>
-                                        <li>
-                                            {char.mass}
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                                <Details> 
+                                    <p>
+                                        Birth Year: {char.birth_year}
+                                    </p>
+                                    <p>
+                                        Gender: {char.gender}
+                                    </p>
+                                    <p>
+                                        Height: {char.height}
+                                    </p>
+                                    <p>
+                                        Mass: {char.mass}
+                                    </p>
+                                </Details>
+                            </CharacterCard>
                         )
                     })
                 }
-            </div>
             <div>
-                <button onClick={() => page > 1 ? setPage(page-1) : alert('On Page 1')}>Previous</button>
-                <button onClick ={() => setPage(page+1)}>Next</button>
+                <Buttons onClick={() => page > 1 ? setPage(page-1) : alert('On Page 1')}>Previous</Buttons>
+                <Buttons onClick ={() => setPage(page+1)}>Next</Buttons>
             </div>
-        </div>
+        </CharactersList>
 
     )
 }
